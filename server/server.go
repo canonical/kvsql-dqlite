a37d4918dbcaef3147c41ce652fab879915d2950
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-04-27 10:10:43 +0300

    Increase the bootup timeout from 1 min to 5

--- server/server.go
+++ server/server.go
@@ -86,7 +86,7 @@ func New(dir string) (*Server, error) {
 		}
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Minute)
 	defer cancel()
 
 	if err := app.Ready(ctx); err != nil {
